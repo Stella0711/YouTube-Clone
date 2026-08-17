832ea8b46567dac8d5cd41c272137138c9f56dcc
Stella0711 <crystal.quigley@gmail.com>
2026-08-16 23:12:40 -0500

    build navbar structure and global CSS

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import './Navbar.css'
 import menu_icon from '../../assets/menu.png'
 import logo from '../../assets/logo.png'
-import 
+import search_icon from '../../assets/search.png'
+import upload_icon from '../../assets/upload.png'
+import more_icon from '../../assets/more.png'
+import notification_icon from '../../assets/notification.png'
+import profile_icon from '../../assets/jack.png'
 
 const Navbar = () => {
   return (
-    <nav ClassName='flex-div'>
+    <nav className='flex-div'>
         <div className='nav-left flex-div'>
             <img className='menu-icon' src={menu_icon} alt="" />
             <img className ='logo' src={logo} alt="" />
@@ -15,6 +19,13 @@ const Navbar = () => {
         <div className="nav-middle flex-div">
             <input type="text" placeholder='Search'/>
             <img src={search_icon} alt="" />
+        </div>
+
+        <div className="nav-right flex-div">
+            <img src={upload_icon} alt="" />
+            <img src={more_icon} alt="" />
+            <img src={notification_icon} alt="" />
+            <img src={profile_icon} className='user_icon' alt="" />
         </div>
     </nav>
   )
